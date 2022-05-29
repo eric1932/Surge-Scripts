934f1c8c36b9b8d6de1d98835e2b5c0092192d1b
Eric Liu <eric1932777@gmail.com>
2022-05-29 17:46:47 -0500

    [feat] Prevent unnecessary switch by comparing with current policy

--- Bilibili/anime_set_proxy.js
+++ Bilibili/anime_set_proxy.js
@@ -15,6 +15,19 @@ const SWITCH_REGEX = /^https:\/\/www\.bilibili\.com\/(anime|bangumi\/play)/;
 
 
 
+// get current state to prevent unnecessary switch
+let promiseCurrentPolicy = new Promise(function (resolve, reject) {
+    $httpAPI(
+        'GET',
+        `/v1/policy_groups/select?group_name=${encodeURI(TARGET_POLICY_GROUP)}`,
+        null,
+        result =>
+            // response example: {"policy": "ProxyA"}
+            resolve(result.policy)
+    )
+})
+
+// find target policy
 let apiBody = {
     'group_name': TARGET_POLICY_GROUP,
     'policy': undefined,  // default?
@@ -30,14 +43,23 @@ if (SWITCH_REGEX.test($request.url)) {
     notificationMessage = '关闭B站代理';
 }
 
-$httpAPI('POST', '/v1/policy_groups/select', apiBody, (result) => {
-    if (result === null) {
-        $notification.post(notificationMessage, $request.url, '');
+
+
+promiseCurrentPolicy.then(currentPolicy => {
+    if (currentPolicy === apiBody['policy']) {
+        // no need to switch
+        console.log(`${$request.url}，无需切换`);
         $done();
     } else {
-        // on success: result === null
-        // on failure: result === {"error" : "invalid parameters"}
-        $notification.post(notificationMessage, $request.url, result.error);
-        $done();
+        // perform switch
+        $httpAPI('POST', '/v1/policy_groups/select', apiBody, (result) => {
+            if (result === null)
+                $notification.post(notificationMessage, $request.url, '');
+            else
+                // on success: result === null
+                // on failure: result === {"error" : "invalid parameters"}
+                $notification.post(notificationMessage, $request.url, result.error);
+            $done();
+        })
     }
 })
